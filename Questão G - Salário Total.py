04d439572a97c893a50b648dcde9f067627b1991
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-03-31 02:19:14 -0300

    Add files via upload

---
+++ Questão G - Salário Total.py
@@ -0,0 +1,7 @@
+horas = float(input("Digite as horas trabalhadas: ")) # Recebe as horas trabalhadas que o usuario coloca
+valor = float(input("Digite o valor da hora trabalhada: ")) # Recebe o valor de hora trabalhada que o usuario coloca
+salario = horas * valor # Calcula o salário total multiplicando as horas trabalhadas pelo valor da hora
+
+print("Salário Total:", salario)  # Printa o resultado do salário total 
+
+
