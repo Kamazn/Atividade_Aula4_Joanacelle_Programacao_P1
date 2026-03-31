46a85aa45cbda3356e4a318b4dd09e1a28181a41
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-03-31 01:36:07 -0300

    Delete questão1.py

--- questão1.py
+++
@@ -1,5 +0,0 @@
-numero1 = float(input("Digite o primeiro número: ")) # Recebe o primeiro valor que o usuario coloca
-numero2 = float(input("Digite o segundo número: ")) # Recebe o segundo valor que o usuario coloca
-resultado = numero1 * numero2 # Multiplica os dois valores que o usuario colocou
-
-print("Resultado:", resultado) # Printa o resultado dos dois valores multiplicados
