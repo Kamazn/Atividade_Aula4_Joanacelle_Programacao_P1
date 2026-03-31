bd8beda5dad73690fc316f5759c06874c48e42b1
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-03-31 02:10:28 -0300

    Add files via upload

---
+++ Questão F - Conversão de Fahrenheit.py
@@ -0,0 +1,5 @@
+celsius = float(input("Digite a temperatura em Celsius: ")) # Recebe o valor da temperatura em graus Celsius que o usuario coloca
+conversao = celsius * 1.8 + 32 # Converte o valor de celsius para fahrenheit, multiplicando o celsius por 1.8 e somando + 32
+
+print("Valor convertido em Fahrenheit:", conversao) # Printa a conversão feita para fahrenheit
+
