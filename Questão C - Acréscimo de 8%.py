0c18f94a5b74f5df40099fd49600ca298feeea11
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-03-31 01:50:53 -0300

    Add files via upload

---
+++ Questão C - Acréscimo de 8%.py
@@ -0,0 +1,4 @@
+produto = float(input("Digite o valor do produto:")) # Recebe o valor do produto que o usuario coloca
+precofinal = produto + (produto * 0.08) # Soma o valor do produto + 8% de imposto (do valor dele)
+
+print("Preço final:", precofinal) # Printa o resultado do preço final, somado com os 8%
