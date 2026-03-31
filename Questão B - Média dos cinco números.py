9c82048f72aa38a4b4ff94e16346eca62eca84c2
João Gabriel Gonçalves <gameplayb917@gmail.com>
2026-03-31 01:44:31 -0300

    Add files via upload

---
+++ Questão B - Média dos cinco números.py
@@ -0,0 +1,8 @@
+numero1 = float(input("Digite o primeiro número:")) # Recebe o primeiro valor que o usuario coloca
+numero2 = float(input("Digite o segundo número:")) # Recebe o segundo valor que o usuario coloca
+numero3 = float(input("Digite o terceiro número:")) # Recebe o terceiro valor que o usuario coloca
+numero4 = float(input("Digite o quarto número:")) # Recebe o quarto valor que o usuario coloca 
+numero5 = float(input("Digite o quinto número:")) # Recebe o quinto valor que o usuario coloca
+media = (numero1 + numero2 + numero3 + numero4 + numero5) / 5 # Soma todos e divide pelo numero de valores que o usuario colocou (média aritmética)
+
+print("Resultado:", media) # Printa o resultado da média aritmética
